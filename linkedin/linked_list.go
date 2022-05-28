e889167a1b5d314963b30fe447e3aec486f88839
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2022-05-27 21:47:46 -0400

    lclls

--- linkedin/linked_list.go
+++ linkedin/linked_list.go
@@ -7,13 +7,19 @@ type No struct {
 	Braco *No
 }
 
-func (n *No) Apontar(outro *No) {
-	n.Braco = outro
+func (n *No) Apontar(outros ...*No) {
+	if len(outros) == 0 {
+		return
+	}
+
+	primeiro := outros[0]
+	n.Braco = primeiro
+	primeiro.Apontar(outros[1:]...)
 }
 
 func Imprimir(n *No) {
 	for n != nil {
-		fmt.Printf("%d -> ", n.Valor)
+		fmt.Printf("%d ->", n.Valor)
 
 		n = n.Braco
 	}
--- linkedin/linked_list_test.go
+++ linkedin/linked_list_test.go
@@ -1,16 +1,16 @@
 package linkedin
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLinkedList(t *testing.T) {
+
 	a := &No{Valor: 1}
 	b := &No{Valor: 2}
 	c := &No{Valor: 3}
 	d := &No{Valor: 4}
 
-	a.Apontar(b)
-	b.Apontar(c)
-	c.Apontar(d)
-
+	a.Apontar(b, c, d)
 	Imprimir(a)
 }
